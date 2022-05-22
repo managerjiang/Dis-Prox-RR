11ebf4e3570c070a7a30b4bafab36e5f610db6f5
managerjiang <jx13032828@163.com>
2022-05-22 17:09:35 +0800

    Add files via upload

---
+++ w8a - convex-invariant/fungk.m
@@ -0,0 +1,19 @@
+function [fv,gkv,XLX]=fungk(xstore,xx,grad,A,L,C)
+fval =0.28;
+  lamuda1=5*10^(-4);
+    fv=0;
+    gkv=0;
+    fi=sum(log((1+exp(-L'.*A*xx))),1);
+    fv=fi/size(A,1)+lamuda1*norm(xx,1)-fval;%+lamuda2*norm(x_k_1,2)^2;         
+    g_k=grad+lamuda1*sign(xx);
+    gkv=norm(g_k);
+   XLX=0;
+   agent_num=size(C,1);
+  for i=1:agent_num
+    temp_X=zeros(size(A,2),1);
+    for j=1:agent_num
+        temp_X=temp_X+C(i,j)*(xstore(:,i)-xstore(:,j));
+    end
+    XLX=XLX+xstore(:,i)'*temp_X;
+  end
+end
